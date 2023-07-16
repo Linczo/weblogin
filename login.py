fa51f4ed287d19684fd53d74ca34838fdcb27f8b
Linczo <138788675+Linczo@users.noreply.github.com>
2023-07-16 11:24:49 +0800

    Update login.py

--- login.py
+++ login.py
@@ -9,7 +9,65 @@ users = [
 ]
 
 async def login(username, password, browser):
-    # Rest of the code...
+    retry_count = 0
+    success = False
+
+    while retry_count < 3 and not success:
+        try:
+            page = await browser.newPage()
+            await page.goto('http://wwww.cq17.com:12345/index/Index/Userlogins.html')
+
+            # Wait for page to load
+            await page.waitForSelector('#content_name')
+            await page.waitForSelector('#content_password')
+            await page.waitForSelector('.content_button button')
+
+            # Enter username and password
+            await page.type('#content_name', username)
+            await page.type('#content_password', password)
+
+            # Submit login form
+            await page.click('.content_button button')
+
+            # Wait for successful login
+            await page.waitForNavigation()
+
+            # Check if login was successful
+            if 'User/index.html' in page.url:
+                print(f'Successful login for user: {username}')
+
+                # Go to user center page
+                await page.goto('http://wwww.cq17.com:12345/index/User/index.html')
+
+                # Wait for page to load
+                await page.waitForSelector('.signinqd')
+
+                # Click on red envelope to claim
+                await page.click('.signinqd')
+
+                # Perform other operations after claiming the red envelope...
+
+                print(f'Claimed red envelope for user: {username}')
+
+                # Click on safe logout
+                await page.click('.quit a')
+
+                # Wait for logout to complete
+                await page.waitForNavigation()
+
+                print(f'Logged out for user: {username}')
+
+                success = True
+            else:
+                # Retry if login failed
+                retry_count += 1
+                await page.close()
+        except Exception as e:
+            print(f"An error occurred during login for user {username}: {str(e)}")
+            retry_count += 1
+
+    # Random delay
+    await asyncio.sleep(delay_time())
 
 async def main():
     browser = None  # Initialize browser variable with None
